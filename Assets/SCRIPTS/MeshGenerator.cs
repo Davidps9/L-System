7fc158cd2ba5cdd6c6a16ecfa113a48decc4be41
Davidps9 <david.pujol.santander@gmail.com>
2023-10-04 14:12:30 +0200

    continuing meshing

--- Assets/SCRIPTS/MeshGenerator.cs
+++ Assets/SCRIPTS/MeshGenerator.cs
@@ -3,6 +3,8 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
@@ -13,50 +15,68 @@ public class MeshGenerator : MonoBehaviour
 
     List<Vector3> vertices = new List<Vector3>();
     List <int> triangles = new List<int>();
-
+    [SerializeField] int vertexDepth = 4;
     private void Start()
     {
         mesh = GetComponent<MeshFilter>().mesh;
     }
     public void GenerateVertex(Vector3 position)
-    {
+    {     
+
+
         vertices.Add(position);
         vertices.Add(position + Vector3.right);
-        vertices.Add(position + Vector3.forward);
-        vertices.Add(position + Vector3.forward + Vector3.right);
-        if (vertices.Count > 4)
+        vertices.Add(position + Vector3.back);
+        vertices.Add(position + Vector3.back + Vector3.right);
+
+        if (vertices.Count > vertexDepth)
         {
 
-            triangles.Add(vertices.IndexOf(position) + 2);
-            triangles.Add(vertices.IndexOf(position));
-            triangles.Add(vertices.IndexOf(position) + 1);
-            triangles.Add(vertices.IndexOf(position) + 2);
-            triangles.Add(vertices.IndexOf(position) + 3);
-            triangles.Add(vertices.IndexOf(position) + 1);
-            triangles.Add(vertices.IndexOf(position));
-            triangles.Add(vertices.IndexOf(position) - 4 );
-            triangles.Add(vertices.IndexOf(position) - 3);
-            triangles.Add(vertices.IndexOf(position) + 1);
-            triangles.Add(vertices.IndexOf(position));            
-            triangles.Add(vertices.IndexOf(position) - 3);
+            AddBase(vertices.IndexOf(position));
+
+            //for (int i = 0; i < vertexDepth; i++)
+            //{
+                AddWall(vertices.IndexOf(position), 0);
+                AddWall(vertices.IndexOf(position), 1);
+               
+            //}
+
+
+
+
         }
         else
         {
-            triangles.Add(vertices.IndexOf(position));
-            triangles.Add(vertices.IndexOf(position) + 1);
-            triangles.Add(vertices.IndexOf(position) + 2);
-            triangles.Add(vertices.IndexOf(position) + 2);            
-            triangles.Add(vertices.IndexOf(position) + 3);
-            triangles.Add(vertices.IndexOf(position) + 1);
+            AddBase(vertices.IndexOf(position));
         }
        
     }
 
+    private void AddWall(int index,int offset)
+    {
+        int res = 1 + offset;
+        triangles.Add(index  + offset);
+        triangles.Add(index - (4 - offset));
+        triangles.Add(index - (3 + offset));
+        triangles.Add(index + res);
+        triangles.Add(index + offset);
+        triangles.Add(index - (3 + offset));
+    }
 
     public void UpdateMesh()
     {
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
         mesh.RecalculateNormals();
+    }
+
+    private void AddBase(int index)
+    {
+        triangles.Add(index);
+        triangles.Add(index + 2);
+        triangles.Add(index + 3);
+        triangles.Add(index);
+        triangles.Add(index + 3);
+        triangles.Add(index + 1);
     }
 }
