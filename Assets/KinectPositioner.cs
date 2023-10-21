6f695c2af57eeaceef69f1ad96bef870065b3e4a
Pau <pauguvi@gmail.com>
2023-10-21 13:39:06 +0200

    Show kinect actual rendering area

--- Assets/KinectPositioner.cs
+++ Assets/KinectPositioner.cs
@@ -1,17 +1,33 @@
+using UnityEditor;
 using UnityEngine;
 
 public class KinectPositioner : MonoBehaviour
 {
+    [SerializeField] private float fov = 60;
+    [SerializeField] private float nearPlane = 0.2f;
+    [SerializeField] private float farPlane = 5f;
+
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.green;
+        // Kinect position
+        Gizmos.color = new Color(0, 1, 0, 0.4f);
         Gizmos.DrawSphere(transform.position, 0.2f);
+
+        if (Selection.Contains(gameObject)) { return; }
+
+        // kinect mirrored frustum
+        Gizmos.color = Color.green;
+        Vector3 mirroredPosition = new Vector3(transform.position.x, transform.position.y, -1 * transform.position.z);
+        Quaternion mirroredRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, -transform.rotation.eulerAngles.y + 180, transform.rotation.eulerAngles.z);
+        Gizmos.matrix = Matrix4x4.TRS(mirroredPosition, mirroredRotation, transform.localScale);
+        Gizmos.DrawFrustum(Vector3.zero, fov, farPlane, nearPlane, 1.2f);
     }
 
     private void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.green;
+        // kinect real frustum
+        Gizmos.color = new Color(0, 1, 0, 0.4f);
         Gizmos.matrix = transform.localToWorldMatrix;
-        Gizmos.DrawFrustum(Vector3.zero, 60, 5, 0.2f, 1.2f);
+        Gizmos.DrawFrustum(Vector3.zero, fov, farPlane, nearPlane, 1.2f);
     }
 }
