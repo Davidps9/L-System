c3557e6623e316378be32dda3d0357461498c4ed
Davidps9 <david.pujol.santander@gmail.com>
2023-10-25 14:59:15 +0200

    not important changes

--- Assets/SCRIPTS/Fish Simulation/ScriptsLocos/ObstacleAvoidance.cs
+++ Assets/SCRIPTS/Fish Simulation/ScriptsLocos/ObstacleAvoidance.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ObstacleAvoidance : MonoBehaviour
 {
@@ -11,25 +12,29 @@ public class ObstacleAvoidance : MonoBehaviour
     [Header ("Math Components")]
     [SerializeField] int numOfPoints;
     [SerializeField] float turnFraction;
-    [SerializeField] float positionDistance;
+    [SerializeField] float turnFractionSpeed;
+    [SerializeField] float radius;
     [SerializeField] float highlightOffset;
     [SerializeField] float highlightValue;
-    [SerializeField] float powPower;
+    [Header ("Montaje truco")]
+    [SerializeField] GameObject cameraObj;
+    [SerializeField]private float angle;
+
     private List<GameObject> obstacles = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        GenerateFOV();
+        StartCoroutine(GenerateFOV());
     }
 
     // Update is called once per frame
     void Update()
     {
-        turnFraction += 0.000001f;
-        CalculatePosition();
+
+
     }
 
-    void GenerateFOV()
+    IEnumerator GenerateFOV()
     {
         
         for (int i = 0; i < numOfPoints; i++)
@@ -38,12 +43,17 @@ public class ObstacleAvoidance : MonoBehaviour
             float inclination = Mathf.Acos (1 -2 *t);
             float azimuth = 2 * Mathf.PI * turnFraction * i;
 
-            float x =Mathf.Sin(inclination) * Mathf.Cos(azimuth);
-            float y = Mathf.Sin(inclination) * Mathf.Sin(azimuth);
-            float z = Mathf.Cos(inclination);
+            float x =Mathf.Sin(inclination) * Mathf.Cos(azimuth) * radius;
+            float y = Mathf.Sin(inclination) * Mathf.Sin(azimuth) * radius;
+            float z = Mathf.Cos(inclination) * radius;
 
             Vector3 pos = new Vector3(x, y, z);
-            GameObject clone = Instantiate(prefab, pos, Quaternion.identity);
+            GameObject clone = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+            while(clone.transform.position != pos)
+            {
+                clone.transform.Translate(pos);
+                yield return new WaitForEndOfFrame();
+            }
             clone.transform.parent = parent.transform;
             Material mat;
             
@@ -54,27 +64,40 @@ public class ObstacleAvoidance : MonoBehaviour
 
             }
             obstacles.Add(clone);
-
+            yield return new WaitForEndOfFrame();
 
         }
         prefab.SetActive(false);
+        StartCoroutine(CalculatePosition());
+
     }
 
-    void CalculatePosition()
+    IEnumerator CalculatePosition()
     {
-        for(int i = 0;i < numOfPoints;i++)
-        {
-            float t = i / (numOfPoints - 1f);
-            float inclination = Mathf.Acos(1 - 2 * t);
-            float azimuth = 2 * Mathf.PI * turnFraction * i;
+        while (true) { 
+            turnFraction += turnFractionSpeed;
 
-            float x = Mathf.Sin(inclination) * Mathf.Cos(azimuth) * positionDistance;
-            float y = Mathf.Sin(inclination) * Mathf.Sin(azimuth) * positionDistance;
-            float z = Mathf.Cos(inclination) * positionDistance;
+            for (int i = 0;i < numOfPoints;i++)
+            {
+                float t = i / (numOfPoints - 1f);
+                float inclination = Mathf.Acos(1 - 2 * t);
+                float azimuth = 2 * Mathf.PI * turnFraction * i;
 
-            Vector3 pos = new Vector3(x, y, z);
+                float x = Mathf.Sin(inclination) * Mathf.Cos(azimuth) * radius;
+                float y = Mathf.Sin(inclination) * Mathf.Sin(azimuth) * radius;
+                float z = Mathf.Cos(inclination) * radius;
 
-            UpdatePsition(i, pos);
+                Vector3 pos = new Vector3(x, y, z);
+
+                UpdatePsition(i, pos);
+               // Debug.DrawRay(parent.transform.position, pos, Color.green);
+            }
+            yield return new WaitForEndOfFrame();
+
+            cameraObj.transform.RotateAround(parent.transform.position, Vector3.up, angle);
+            cameraObj.transform.LookAt(parent.transform.position);
+            Debug.Log(angle);
+            Debug.Log(cameraObj.transform.rotation);
         }
     }
 
@@ -82,8 +105,10 @@ public class ObstacleAvoidance : MonoBehaviour
     void UpdatePsition(int i, Vector3 targetPos)
     {
         
-            obstacles[i].transform.position = targetPos;
+            obstacles[i].transform.localPosition = targetPos;
 
         
     }
+
+    
 }
